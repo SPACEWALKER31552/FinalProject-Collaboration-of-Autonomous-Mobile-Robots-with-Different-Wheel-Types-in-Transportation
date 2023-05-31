695c82a540e185fab31836cc55282a8ac6f33b54
SPACEWALKER <109845426+SPACEWALKER31552@users.noreply.github.com>
2023-05-31 10:35:43 +0000

    Add files via upload

---
+++ Holonomic_2/pete_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export PKG_CONFIG_PATH="/home/ubuntu/pete_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/ubuntu/pete_ws/build'
+export PYTHONPATH="/home/ubuntu/pete_ws/devel/lib/python3/dist-packages:$PYTHONPATH"
